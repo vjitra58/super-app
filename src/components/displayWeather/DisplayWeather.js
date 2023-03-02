a3f52e9227be86a6691f2c5df14eac1fe5a24bd9
vjitra58 <vikaskumar58616@gmail.com>
2023-03-02 19:02:00 +0530

    added the notes

--- src/components/displayWeather/DisplayWeather.js
+++ src/components/displayWeather/DisplayWeather.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from "./style.module.css"
 
 
+
 const DisplayWeather = ({name, val, unit, icon}) => {
   return (
     <div className={styles.container}>
--- src/pages/dashBoard/DashBoard.js
+++ src/pages/dashBoard/DashBoard.js
@@ -3,25 +3,34 @@ import styles from "./DashBoard.module.css";
 import Profile from "../../components/profile/Profile.js";
 import News from "../../components/news/News.js";
 import Weather from "../../components/weather/Weather.js";
-
+import Notes from "../../components/notes/Notes.js";
 
 const DashBoard = () => {
    
   return (
     <div className={styles.container}>
-        <div className={styles.left}>
+      <div className={styles.left}>
+        <div className={styles.top}>
+          <div className={styles.topLeft}>
             <div className={styles.profile}>
                 <Profile />
             </div>
             <div className={styles.weather}>
                 <Weather />
             </div>
+          </div>
+          <div className={styles.topRight}>
+            <Notes />
+          </div>
         </div>
-        <div className={styles.right}>
-            <News />
-        </div>
+        <div className={styles.bottom}></div>
+      </div>
+      <div className={styles.right}>
+        <News/>
+      </div>
     </div>
-  )
+  );
 }
+
 
 export default DashBoard
